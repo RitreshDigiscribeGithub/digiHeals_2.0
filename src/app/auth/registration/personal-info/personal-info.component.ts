f8e4097c66db0ec3b7262122910399eaad3c0fed
RitreshDigiscribeGithub <ritesh@digiscribe.in>
2021-09-01 12:39:21 +0530

    change data input

--- src/app/auth/registration/personal-info/personal-info.component.ts
+++ src/app/auth/registration/personal-info/personal-info.component.ts
@@ -8,11 +8,36 @@ import { Component, OnInit } from '@angular/core';
 export class PersonalInfoComponent implements OnInit {
 
   part = 1;
+  date = new Date();
+
   constructor() { }
   onChange(event) {
     console.log(event);
   }
+
+
+  dateObj = {
+    day: () => this.getDay(),
+    month: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    years: () => this.getYear()
+  }
   ngOnInit(): void {
+  }
+
+  getDay() {
+    let day = []
+    for (let i = 1; i <= 31; i++) {
+      day.push(i)
+    }
+    return day;
+  }
+
+  getYear() {
+    let year = [];
+    for (let i = 1970; i <= this.date.getFullYear(); i++) {
+      year.push(i)
+    }
+    return year;
   }
 
 }
